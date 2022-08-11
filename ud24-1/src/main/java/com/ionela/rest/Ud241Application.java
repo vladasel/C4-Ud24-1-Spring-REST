5429a010f41001a448875a6a6af3e3a9b3c50c55
vladasel <ionelavladasel2710@gmail.com>
2022-08-11 10:08:38 +0200

    created project

---
+++ ud24-1/src/main/java/com/ionela/rest/Ud241Application.java
@@ -0,0 +1,13 @@
+package com.ionela.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud241Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud241Application.class, args);
+	}
+
+}
---
+++ ud24-1/src/test/java/com/ionela/rest/Ud241ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionela.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud241ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
